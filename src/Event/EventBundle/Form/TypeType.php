c62b8726d2f032db04e5ab1f1a7cc807d884728f
Pohaki <kevin.lachaise@gmail.com>
2016-05-20 10:29:39 +0200

    Add Public and Type to create form

---
+++ src/Event/EventBundle/Form/TypeType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Event\EventBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class TypeType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        ->add('type', 'entity', array('class' => 'Event\EventBundle\Entity\Type',
+        'property' => 'title',
+        'multiple' => false,
+        'empty_value' => 'Type d\'évenement'))
+        ;
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Event\EventBundle\Entity\Type'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'event_eventbundle_type';
+    }
+}
