4099d77b81cb84c0b4bfadb742ccc61b91b0f357
Flavien94 <favien_94@hotmail.fr>
2016-05-26 09:28:16 +0200

    register without email

--- src/Users/UsersBundle/Entity/Users.php
+++ src/Users/UsersBundle/Entity/Users.php
@@ -7,6 +7,19 @@ use Doctrine\ORM\Mapping as ORM;
 /**
 * @ORM\Entity
 * @ORM\Table(name="users")
+* @ORM\AttributeOverrides({
+*     @ORM\AttributeOverride(name="email",
+*          column=@ORM\Column(
+*              nullable = true
+*          )
+*      ),
+*     @ORM\AttributeOverride(name="emailCanonical",
+*          column=@ORM\Column(
+*              name = "email_canonical",
+*              nullable = true
+*          )
+*      )
+* })
 */
 class Users extends BaseUser
 {
@@ -17,7 +30,6 @@ class Users extends BaseUser
     * @ORM\GeneratedValue(strategy="AUTO")
     */
    protected $id;
-
 
    public function setUsername($username){
        parent::setUsername($username);
