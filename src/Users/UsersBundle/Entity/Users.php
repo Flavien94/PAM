27a135b8bce0470ba63585ab668f73681940c020
Flavien94 <favien_94@hotmail.fr>
2016-05-27 10:59:25 +0200

    real registration without email i know..

--- src/Users/UsersBundle/Entity/Users.php
+++ src/Users/UsersBundle/Entity/Users.php
@@ -4,37 +4,19 @@ namespace Users\UsersBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+
 /**
-* @ORM\Entity
-* @ORM\Table(name="users")
-* @ORM\AttributeOverrides({
-*     @ORM\AttributeOverride(name="email",
-*          column=@ORM\Column(
-*              nullable = true
-*          )
-*      ),
-*     @ORM\AttributeOverride(name="emailCanonical",
-*          column=@ORM\Column(
-*              name = "email_canonical",
-*              nullable = true
-*          )
-*      )
-* })
+ * @ORM\Entity
+ * @ORM\Table(name="users")
 */
 class Users extends BaseUser
 {
-
    /**
     * @ORM\Id
     * @ORM\Column(type="integer")
     * @ORM\GeneratedValue(strategy="AUTO")
     */
    protected $id;
-
-   public function setUsername($username){
-       parent::setUsername($username);
-       $this->setEmail(null);
-   }
 
    public function __construct()
    {
